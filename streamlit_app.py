e9e8a5132da73b119b1a9290b2c18fc8ad2bd9a2
PHILIP NEELY <122125662+neelypm02@users.noreply.github.com>
2023-01-20 10:17:46 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.title('My Moms Healthy Diner')
+streamlit.title('My Moms New Healthy Diner')
 streamlit.header('Breakfast Faves')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
